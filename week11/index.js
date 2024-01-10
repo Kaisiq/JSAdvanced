5ba25cafe68a1c73aac83e7c0cc95b67d0211868
kaisiq <dahudohu@gmail.com>
2024-01-10 19:19:45 +0200

    Add persistant storage for messages and usernames for users

--- week11/index.js
+++ week11/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -10,29 +10,42 @@ const bodyParser = require("body-parser");
 
 let connectedSockets = [];
 
+let messages = [];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 io.on("connection", (socket) => {
-  connectedSockets = connectedSockets.concat(socket);
+    connectedSockets = connectedSockets.concat(socket);
 
-  setInterval(() => {
-    socket.emit("system-message", "Hello from server");
-  }, 2000);
+    socket.emit("request-username");
+    let username = "";
 
-  socket.on("chat-message", (message) => {
-    io.emit("chat-message", message);
-  });
+    socket.on("entered-username", (newUsername) => {
+        username = newUsername;
+        socket.emit("render");
+        messages.forEach((message) => {
+            socket.emit("chat-message", message);
+        });
+    });
 
-  socket.on("disconnected", () => {
-    connectedSockets = connectedSockets.filter(s => s !== socket);
-  });
+    // setInterval(() => {
+    //     socket.emit("system-message", "Hello from server");
+    // }, 2000);
+
+    socket.on("chat-message", (message) => {
+        messages.push(username + "> " + message);
+        io.emit("chat-message", username + "> " + message);
+    });
+
+    socket.on("disconnected", () => {
+        connectedSockets = connectedSockets.filter((s) => s !== socket);
+        username = "";
+    });
 });
 
-
-
 server.listen(8080, () => {
-  console.log("Server is listening on :8080");
+    console.log("Server is listening on :8080");
 });
